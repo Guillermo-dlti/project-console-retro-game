6da391077f224bcd967f3bc173747080de5e384e
Guillermo de la Torre Isunza <memo2507.delatorre@gmail.com>
2026-04-08 15:24:37 -0600

    Arreglando colores y joysticks

--- src/components/RightControl.jsx
+++ src/components/RightControl.jsx
@@ -4,21 +4,25 @@
 
 import React from 'react'
 
-function RightControl({handleSelection}) {
+function RightControl({ handleSelection }) {
     return (
-        <div className="w-[100px] h-[250px] border-4 border-solid  relative bg-red-500 rounded-r-3xl">
+        <div className="w-[100px] h-[250px] border-4 border-solid  relative bg-[#ff5d51] rounded-r-3xl">
             <div className="absolute -top-4 right-2 w-20 h-4 bg-black rounded-t-xl"></div>
 
             <div className="absolute -top-2 left-4 -translate-x-1/2 text-4xl font-extrabold">+</div>
 
-            <div className="absolute top-10 left-1/2 -translate-x-1/2 w-14 h-14 relative">
-                <div className="absolute top-0 left-1/2 -translate-x-1/2 w-5 h-5 border-2 rounded-full flex items-center justify-center text-xs">X</div>
-                <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-5 h-5 border-2 rounded-full flex items-center justify-center text-xs">B</div>
-                <div className="absolute left-0 top-1/2 -translate-y-1/2 w-5 h-5 border-2 rounded-full flex items-center justify-center text-xs">Y</div>
-                <div onClick={handleSelection} className="absolute right-0 top-1/2 -translate-y-1/2 w-5 h-5 border-2 rounded-full flex items-center justify-center text-xs">A</div>
+            <div className="absolute top-10 left-1/2 -translate-x-1/2 w-19 h-19 relative">
+                <div className="absolute top-0 left-1/2 -translate-x-1/2 w-7 h-7 bg-black text-white rounded-full flex items-center justify-center text-sm">X</div>
+                <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-7 h-7 bg-black text-white rounded-full flex items-center justify-center text-sm">B</div>
+                <div className="absolute left-0 top-1/2 -translate-y-1/2 w-7 h-7 bg-black text-white rounded-full flex items-center justify-center text-sm">Y</div>
+                <div onClick={handleSelection} className="absolute right-0 top-1/2 -translate-y-1/2 w-7 h-7 bg-black text-white rounded-full flex items-center justify-center text-sm">A</div>
             </div>
 
-            <div className="absolute bottom-8 left-1/2 -translate-x-1/2 w-15 h-15 border-4 rounded-full"></div>
+            {/* <div className="absolute bottom-8 left-1/2 -translate-x-1/2 w-15 h-15 border-4 rounded-full"6</div> */}
+
+            <div className="absolute bottom-8 left-1/2 -translate-x-1/2 w-16 h-16 rounded-full flex items-center justify-center bg-zinc-800  border-4">
+                <div className="w-12 h-12 rounded-full bg-gradient-to-b from-zinc-400 to-zinc-600"></div>
+            </div>
 
         </div>
     )
