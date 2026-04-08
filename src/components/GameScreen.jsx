1b76cac89618a122b8de45e3158ddba32b78d065
Guillermo de la Torre Isunza <memo2507.delatorre@gmail.com>
2026-04-08 15:00:55 -0600

    Styling GameScreen

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -5,27 +5,27 @@ function GameScreen({ myPokeSelection, pcPokeSelection }) {
     <>
       <div className="w-[440px] h-[250px] border-y-4 border-solid">
         <div className="flex flex-col items-center justify-center">
-          <h1 className='mb-8 text-2xl font-extrabold'>Pokemon Battle</h1>
-          <div className='flex  items-center gap-14'>
+          <h1 className='mt-2 mb-4 text-2xl font-extrabold'>Pokemon Battle</h1>
+          <div className='flex  items-center gap-8'>
             {myPokeSelection?.map((pokemon, index) => (
-              <div key={index} className="flex flex-col" >
+              <div key={index} className="flex flex-col items-center" >
                 <img
                   src={pokemon?.sprites?.front_default}
                   alt={pokemon.name}
-                  className="w-25 h-25"
+                  className="w-35 h-35"
                 />
-                <p>{pokemon.name}</p>
+                <p className='text-lg'>{pokemon.name}</p>
               </div>
             ))}
-            <h2>VS.</h2>
+            <h2 className='font-bold text-3xl'>VS.</h2>
             {pcPokeSelection?.map((pokemon, index) => (
-              <div key={index} className="flex flex-col" >
+              <div key={index} className="flex flex-col items-center" >
                 <img
                   src={pokemon?.sprites?.front_default}
                   alt={pokemon.name}
-                  className="w-25 h-25"
+                  className="w-35 h-35"
                 />
-                <p>{pokemon.name}</p>
+                <p className='text-lg'>{pokemon.name}</p>
               </div>
             ))}
           </div>
